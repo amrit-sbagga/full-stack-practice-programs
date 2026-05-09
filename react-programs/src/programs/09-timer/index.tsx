3cb62eb734af382b73879d84ec318b2e7803f934
Amrit Singh <amrit.sbagga@gmail.com>
2026-05-09 23:11:21 +0530

    feat: start/stop timer implemented

--- react-programs/src/programs/09-timer/index.tsx
+++ react-programs/src/programs/09-timer/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from 'react';
 /**
  * PROGRAM 09 — Start / Stop Timer
  *
@@ -11,6 +12,57 @@
  *   - The Start button should be disabled while running; Stop disabled while stopped
  */
 
+function formatTime(seconds: number) {
+  const mm = Math.floor(seconds / 60).toString().padStart(2, '0');
+  const ss = (seconds % 60).toString().padStart(2, '0');
+  return `${mm}:${ss}`;
+}
+
 export default function Timer() {
-  return <div />;
+  const [elapsedTime, setElapsedTime] = useState(0)
+  const timerRef = useRef<ReturnType<typeof setInterval> | null>(null); 
+  const [isRunning, setIsRunning] = useState(false);
+  
+  useEffect(() => {
+    return () => {
+     clearInterval(timerRef.current!);
+    }
+  }, [])
+  
+  const startTimer = () => {
+    if(timerRef.current) return;
+    
+    timerRef.current = setInterval(() => {
+      setElapsedTime(prev=> prev + 1);
+    }, 1000);
+    setIsRunning(true);
+  }
+  
+  const stopTimer = () => {
+    clearInterval(timerRef.current!);
+    timerRef.current = null;
+    setIsRunning(false);
+  }
+  
+  const resetTimer = () => {
+    setElapsedTime(0);
+    clearInterval(timerRef.current!);
+    timerRef.current = null;
+    setIsRunning(false);
+  }
+  
+  return <>
+    <p style={{'marginBottom':'16px'}}>{formatTime(elapsedTime)}</p>
+    <button disabled={isRunning}
+      onClick={startTimer} style={{'marginRight':'16px', 'padding': '8px 12px'}}>
+      Start
+    </button>
+    <button disabled={!isRunning}
+      onClick={stopTimer} style={{'marginRight':'16px', 'padding': '8px 12px'}}>
+      Stop
+    </button>
+    <button onClick={resetTimer} style={{'marginRight':'16px', 'padding': '8px 12px'}}>
+      Reset
+    </button>
+  </>;
 }
