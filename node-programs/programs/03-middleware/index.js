245e197c34482d46389d59c312162c52777ff9cb
Amrit Singh <amrit.sbagga@gmail.com>
2026-05-10 21:48:30 +0530

    feat: node middleware program implemented

--- node-programs/programs/03-middleware/index.js
+++ node-programs/programs/03-middleware/index.js
@@ -23,3 +23,57 @@
  *
  * Run: npm run 03
  */
+
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// global logger middleware
+app.use((req, res, next) => {
+  const timestamp = new Date().toISOString();
+  const method = req.method;
+  const path = req.path;
+  console.log(`[${timestamp}] ${method} ${path}`);
+
+  next();
+});
+
+// auth middleware
+const authMiddleWare = (req, res, next) => {
+  const apiKey = req.headers["x-api-key"];
+  if (apiKey !== "secret123") {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+  next();
+};
+
+// endpoints
+app.get("/public", (req, res) => {
+  res.json({ message: "Public route" });
+});
+
+// protected by auth-middleware
+app.get("/private", authMiddleWare, (req, res) => {
+  res.json({ message: "Private route" });
+});
+
+// intentionally throw err to test error handler
+app.get("/crash", (req, res) => {
+  throw new Error("Intentional crash for testing");
+});
+
+// error middleware
+app.use((err, req, res, next) => {
+  res
+    .status(500)
+    .json({ error: "Internal Server Error", message: err.message });
+});
+
+const PORT = 3003;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
